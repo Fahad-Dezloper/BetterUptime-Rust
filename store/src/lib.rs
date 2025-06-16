7be782c58a9a983b5d0d4bc65f869fcc04d2dff2
Fahad <fahad.khan2216@gmail.com>
2025-06-16 16:28:35 +0530

    structure of file improved

--- store/src/lib.rs
+++ store/src/lib.rs
@@ -8,29 +8,5 @@ use crate::config::Config;
 
 pub mod schema;
 pub mod config;
-
-pub struct Store {
-    pub conn: PgConnection
-}
-
-impl Default for Store {
-    fn default() -> Self {
-        let config = Config::default();
-        let conn = PgConnection::establish(&config.database_url)
-            .unwrap_or_else(|_| panic!("Error connecting to {}", config.database_url));
-        Ok(Self { 
-            conn 
-        })
-    }
-}
-
-impl Store {
-    pub fn create_user(&self) {
-        print!("Create User Called");
-        self.conn.execute("INSERT INTO xyz");
-    }
-
-    pub fn create_website(&self) -> String {
-        String::from("1")
-    }
-}
+pub mod store;
+pub mod models;
---
+++ store/src/store.rs
@@ -0,0 +1,16 @@
+use diesel::{Connection, PgConnection};
+
+use crate::config::Config;
+
+pub struct Store {
+    pub conn: PgConnection
+}
+
+impl Default for Store {
+    fn default() -> Self {
+        let config = Config::default();
+        let conn = PgConnection::establish(&config.database_url)
+            .expect("Error connecting to database");
+        Self { conn }
+    }
+}
